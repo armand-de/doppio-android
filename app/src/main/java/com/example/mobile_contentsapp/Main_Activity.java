72550f8e8ed1552a2762f9b7617f21eb37d1bb84
sh596 <sh596@naver.com>
2021-05-29 17:08:57 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mobile_contentsapp/Main_Activity.java
@@ -0,0 +1,19 @@
+package com.example.mobile_contentsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_);
+
+        Intent intent = new Intent(this,Sign_in_Activity.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/mobile_contentsapp/Sign_in_Activity.java
@@ -0,0 +1,43 @@
+package com.example.mobile_contentsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Sign_in_Activity extends AppCompatActivity {
+
+    EditText nicknameedit;
+    EditText passwordedit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+
+        nicknameedit = findViewById(R.id.nickname_edit);
+        passwordedit = findViewById(R.id.password_edit);
+
+        Button signin = findViewById(R.id.singinbtn);
+        Button signup = findViewById(R.id.signupbtn);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(),Signup_Activity.class);
+            startActivity(intent);
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
